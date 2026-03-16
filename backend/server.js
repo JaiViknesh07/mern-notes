35712274daaae462e91448767482cc0ce65bd97f
Jai Viknesh <jaivikneshvembu@gmail.com>
2026-03-16 23:08:18 +0530

    fix render deployment

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ app.use("/api/notes", notesRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("/{*splat}", (req, res) => {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
